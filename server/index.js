9aebdfb46901cadfa20cbdc7cbcf4627676df4d9
Rainer Flores <rainer.flores64@gmail.com>
2024-04-30 11:20:07 -0400

    added sql client

--- server/index.js
+++ server/index.js
@@ -1,13 +1,61 @@
 import express from 'express'
+import logger from 'morgan'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3002
 
 const app = express()
-
-app.get('/', (req, res) => {
-  res.send('<h1> This is real Chat</h1>')
+const server = createServer(app)
+const io = new Server(server, {
+  connectionStateRecovery: {}
 })
 
-app.listen(port, () => {
+const db = createClient({
+  url: 'libsql://normal-calamity-rain.turso.io',
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user TEXT
+  )
+`)
+
+io.on('connection', async (socket) => {
+  console.log('a user has connected!')
+  socket.on('disconnect', () => {
+    console.log('an user has disconnected')
+  })
+
+  socket.on('chat message', async msg => {
+    //console.log('message:' + msg)
+    let result 
+    try {
+      result = await db.execute({
+        sql: `INSERT INTO messages (content) VALUES (:msg)`,
+        args: { msg }
+      })
+    } catch(e) {
+      console.error(e)
+      return
+    }
+    io.emit('chat message', msg, result.lastInsertRowid.toString())
+  })
+})
+
+app.use(logger('dev'))
+
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
+})
+
+server.listen(port, () => {
   console.log(`Server running on port ${port}`)
 })
